472b9d8db97eac5218b1c144efe5c9d1aa918a1e
waageamara <166183052+waageamara@users.noreply.github.com>
2024-05-21 22:48:08 +0200

    prepare sizes

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,10 @@
 // https://github.com/nuxt-themes/alpine/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
+    title: 'Ich bin Jennifer.',
     description: 'The minimalist blog theme',
     image: {
-      src: '/social-card-preview.png',
+      src: '/articles/configure-alpine.webp',
       alt: 'An image showcasing my project.',
       width: 400,
       height: 300
@@ -12,19 +12,19 @@ export default defineAppConfig({
     header: {
       position: 'right', // possible value are : | 'left' | 'center' | 'right'
       logo: {
-        path: '/logo.svg', // path of the logo
+        path: '/logo-dark.svg', // path of the logo
         pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
         alt: 'alpine' // alt of the logo
       }
     },
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
+        enabled: false, // possible value are : true | false
         repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
       },
-      navigation: true, // possible value are : true | false
+      navigation: false, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
+      message: '' // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
       twitter: 'nuxtlabs',
